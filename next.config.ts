fc12faaae89fc939227a5584b8f5d61e67dced2b
aabzed <5393564@qq.com>
2025-09-08 15:00:20 +0800

    ''

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+   experimental: {
+    ppr: 'incremental'
+  },
+  // ... rest of the configuration.
+  output: "standalone",
+
 };
 
 export default nextConfig;
